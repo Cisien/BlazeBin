055da728431f6fab66036b61739b9a8f93b9ab65
Cisien <ccurwick@exoronet.net>
2021-09-03 21:39:16 -0700

    forwarded header config via the docs

--- src/BlazeBin/Server/Startup.cs
+++ src/BlazeBin/Server/Startup.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using System.Text.Json;
 
 using BlazeBin.Client.Services;
@@ -43,6 +44,15 @@ namespace BlazeBin.Server
 
             services.AddHostedService<FileGroomingWorker>();
             services.AddHostedService<StatsCollectionService>();
+
+            services.Configure<ForwardedHeadersOptions>(options =>
+            {
+                options.ForwardedHeaders =ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+                // These three subnets encapsulate the applicable Azure subnets. At the moment, it's not possible to narrow it down further.
+                options.KnownNetworks.Add(new IPNetwork(IPAddress.Parse("::ffff:10.0.0.0"), 104));
+                options.KnownNetworks.Add(new IPNetwork(IPAddress.Parse("::ffff:192.168.0.0"), 112));
+                options.KnownNetworks.Add(new IPNetwork(IPAddress.Parse("::ffff:172.16.0.0"), 108));
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
@@ -54,12 +64,7 @@ namespace BlazeBin.Server
             }
             else
             {
-                app.UseForwardedHeaders(new ForwardedHeadersOptions
-                {
-                    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto,
-                    ForwardedForHeaderName = "X-Forwarded-For",
-                    ForwardedProtoHeaderName = "X-Forwarded-Proto"
-                });
+                app.UseForwardedHeaders();
                 app.UseHttpsRedirection();
                 app.UseHsts();
 
